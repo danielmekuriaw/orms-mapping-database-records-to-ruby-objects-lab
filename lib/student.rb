76a94e5e18fa53e87adc4e6147a230c35e76bb18
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-06-21 00:47:18 -0500

    orms mapping db lab solution

--- lib/student.rb
+++ lib/student.rb
@@ -2,17 +2,35 @@ class Student
   attr_accessor :id, :name, :grade
 
   def self.new_from_db(row)
-    # create a new Student object given a row from the database
+    new_song = self.new  # self.new is the same as running Song.new
+    new_song.id = row[0]
+    new_song.name =  row[1]
+    new_song.grade = row[2]
+    new_song  # return the newly created instance
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+      sql = <<-SQL
+        SELECT *
+        FROM students
+      SQL
+  
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
@@ -40,4 +58,42 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.all_students_in_grade_9
+    array = []
+
+    self.all.each{|student|
+      if student.grade == '9'
+        puts "happened"
+        array << student
+      end
+    }
+
+    array
+  end
+
+  def self.students_below_12th_grade
+    self.all.select{|student|
+      student.grade.to_i < 12
+    }
+  end
+
+  def self.first_X_students_in_grade_10(x)
+    self.all.select{
+    |student| 
+    student.grade.to_i == 10}.first(x)
+  end
+
+  def self.first_student_in_grade_10
+    self.all.select{
+      |student| 
+      student.grade.to_i == 10}.first
+  end
+
+  def self.all_students_in_grade_X(x)
+    self.all.select{
+      |student| 
+      student.grade.to_i == x}
+  end
+
 end
